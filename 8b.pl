16c61dccdd7ff0769f1bc28bf7662e29b53eefb3
E. Choroba <choroba@matfyz.cz>
2020-12-08 10:32:17 +0100

    Fix 8/2: Run the program repeatedly changing the last instruction
    No manual intervention needed.

--- 8b.pl
+++ 8b.pl
@@ -5,24 +5,57 @@ use feature qw{ say };
 
 use ARGV::OrDATA;
 
-my @source = map [split], <>;
-my $acc = 0;
-my $pc = 0;
-my @seen;
+{   package Game::Console;
+    use Moo;
+    has source => (is => 'rw', required => 1);
+    has debug  => (is => 'ro', default => 0);
 
-$source[277][0] = 'nop';
+    my %toggle = (jmp => 'nop', nop => 'jmp');
+    sub fix {
+        my ($self, $addr) = @_;
+        $self->source->[$addr][0] = $toggle{ $self->source->[$addr][0] }
+            or die 'Invalid fix';
+    }
+    sub run {
+        my ($self) = @_;
+        my $acc = 0;
+        my $pc = 0;
+        my @seen;
+        my @trace;
 
-until ($pc > $#source) {
-    my %step = (
-        nop => sub { ++$pc },
-        acc => sub { $acc += $_[0]; ++$pc },
-        jmp => sub { $pc += $_[0] });
-    $seen[$pc] = 1;
-    say "$pc: @{ $source[$pc] } <$acc>";
-    $step{ $source[$pc][0] }->( $source[$pc][1] );
-    die if $seen[$pc];
+        until ($pc > $#{ $self->source }) {
+            my %step = (
+                nop => sub { ++$pc },
+                acc => sub { $acc += $_[0]; ++$pc },
+                jmp => sub { $pc += $_[0] });
+            $seen[$pc] = 1;
+            push @trace, $pc;
+            say "$pc: @{ $self->source->[$pc] } <$acc>" if $self->debug;
+            $step{ $self->source->[$pc][0] }->( $self->source->[$pc][1] );
+            die \@trace if $seen[$pc];
+        }
+        return $acc
+    }
 }
-say $acc;
+
+my @source = map [split], <>;
+
+my $gc0 = 'Game::Console'->new(source => \@source);
+eval { $gc0->run };
+my $trace = $@;
+
+my $last = $#$trace;
+my $result;
+while (1) {
+    my $gc = 'Game::Console'->new(source => \@source);
+    my $last = $#$trace;
+    --$last until $gc->source->[ $trace->[$last] ][0] ne 'acc';
+    $gc->fix($trace->[$last]);
+    splice @$trace, $last;
+    $result = eval { $gc->run };
+    last if defined $result;
+}
+say $result;
 
 __DATA__
 nop +0
